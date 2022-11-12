dc9509a448d9a02916a3e2d7bcaaf4633cdb0a77
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2022-11-12 15:12:04 +0600

    completed all tasks

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// Task 1
+console.log("Task-1\n------------");
+console.log("Sum of 23 and 97 is: " + 23 + 97);
+
+// Task 2
+console.log("\nTask-2\n------------");
+let sumOfNumbers = 0;
+for (let i = 1; i < 7; i++) {
+    sumOfNumbers += i;
+}
+console.log("The sum of 1 to 6 is: " + sumOfNumbers);
+
+// Task 3
+console.log("\nTask-3\n------------");
+console.log("Result = " + (4 + 6 + 9) / 77);
+
+// Task 4 
+console.log("\nTask-4\n------------");
+let a = 10;
+console.log("The value of a is: " + a);
+
+console.log("\n9 * a = " + 9 * a);
+
+let b = 7 * a;
+console.log("\nThe value of b = " + b);
+
+// Task 5
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+console.log("\nPercentage = " + percentage);
